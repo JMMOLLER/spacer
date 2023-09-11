26904bc35938de0ed251a0fc384f220445b9c09a
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-09-11 18:56:50 -0500

    fix input radio and more
    - Se soluciono el problema con los inputs radio.
    - Se mejoró el estilo del carrusel de marcas.
    -Se mejoró el estilo de los cards de productos

--- js/index.js
+++ js/index.js
@@ -25,3 +25,35 @@ cart.addEventListener("click", () => document.querySelector("#popup_cart").class
 
 // agrega el evento click a todos los elementos del menu y evita que se redirija a otra pagina
 document.querySelectorAll("#menuCategorias > li").forEach((el) => el.addEventListener("click", (e) => e.preventDefault()))
+
+// controlador carrusel hero
+function createInterval() {
+  let counter = 0;
+  let interval = setInterval(checkInput.bind({ counter }), 5000);
+  const inputs = document.querySelectorAll("input[type=radio]");
+  inputs.forEach((input) => {
+    input.addEventListener("click", (e) => {
+      counter = Number(e.target.id.split("radio")[1])-1;
+      clearInterval(interval);
+      checkInput.bind({ counter })();
+      interval = setInterval(checkInput.bind({ counter }), 5000);
+    });
+  });
+}
+
+function checkInput() {
+  this.counter++;
+  document.getElementById("radio" + this.counter).checked = true;
+  if (this.counter == 1) {
+    document.querySelector(".carr.first").style.marginLeft = "0px";
+  } else if (this.counter == 2) {
+    document.querySelector(".carr.first").style.marginLeft = "-20%";
+  } else if (this.counter == 3) {
+    document.querySelector(".carr.first").style.marginLeft = "-40%";
+  } else {
+    document.querySelector(".carr.first").style.marginLeft = "-60%";
+    this.counter = 0;
+  }
+}
+
+createInterval();
