4df93dffe6aff319bbf4c74e63caec0e05b7c0b0
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-09-24 20:24:08 -0500

    refactor home paths

--- server/src/main/java/com/example/spacer/spacerbackend/auth/AuthConfig.java
+++ server/src/main/java/com/example/spacer/spacerbackend/auth/AuthConfig.java
@@ -35,7 +35,8 @@ public class AuthConfig {
     return http
       .cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(authorizeRequests ->
-        authorizeRequests.requestMatchers("/api").permitAll().anyRequest().authenticated()
+        authorizeRequests.requestMatchers("/api").permitAll().requestMatchers("/").permitAll()
+          .anyRequest().authenticated()
       ).sessionManagement(sessionManagement ->
         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
--- server/src/main/java/com/example/spacer/spacerbackend/controllers/MainController.java
+++ server/src/main/java/com/example/spacer/spacerbackend/controllers/MainController.java
@@ -6,13 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class MainController {
 
   @GetMapping()
-  public ResponseEntity<?> home() {
+  //redirect to /api
+  public RedirectView home() {
+    return new RedirectView("/api");
+  }
+
+  @GetMapping("/api")
+  public ResponseEntity<?> apiHome() {
     return new ResponseEntity<>(new Response(HttpStatus.OK, HttpStatus.OK.name(), "Welcome to Spacer API 🚀!"), HttpStatus.OK);
   }
 }
