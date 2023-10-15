7d7b5d7c751cfbe5c7e54310a7642fb296e6e002
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-10-14 22:29:48 -0500

    Update cron.js

--- client/api/cron.js
+++ client/api/cron.js
@@ -5,22 +5,26 @@ export const config = {
 export default async function handler(request, response) {
   try {
     const result = await fetch(
-      'https://api.exchangerate.host/convert?from=USD&to=EUR',
+      'https://spacer-api.onrender.com/api',
     );
+
+    if(!result.ok) {
+      throw new Error(result.statusText);
+    }
+
     const data = await result.json();
  
-    if (data.success) {
-      const rate = data.info.rate;
-      console.log(`Latest exchange rate (USD to EUR): ${rate}`);
+    if (data.statusCode === 200) {
+      console.log(data.response);
       response
         .status(200)
-        .json({ message: `Exchange rate (USD to EUR) is ${rate}` });
+        .json({ message: data.description });
     } else {
-      console.error('Error fetching exchange rate:', data);
-      response.status(500).json({ error: 'Failed to fetch exchange rate' });
+      console.log(data.response);
+      response.status(500).json({ error: data.description });
     }
   } catch (error) {
-    console.error('Error fetching exchange rate:', error);
-    response.status(500).json({ error: 'Failed to fetch exchange rate' });
+    console.error('Error fetching spacer api:', error);
+    response.status(500).json({ error: 'Failed to fetch spacer api' });
   }
 }
