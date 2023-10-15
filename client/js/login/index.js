8f72ea7f27b0a45f093ec810d18d2cd2a7569b78
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-10-14 23:50:48 -0500

    fix send form w/o handlesubmit and handle fetchAPI

--- client/js/login/index.js
+++ client/js/login/index.js
@@ -58,6 +58,9 @@ const handleLogin = async (credentials) => {
 window.onload = () => {
   const form = document.querySelector("form");
   form.addEventListener("submit", handleSubmit);
+  
+  form.querySelector("input[type='submit']")
+  .removeAttribute("disabled")
 };
 
 export default function init() {
@@ -66,9 +69,7 @@ export default function init() {
     module.userIsAuth().then((res) =>
     res
       ? (window.location.href = "/")
-      : document
-        .querySelector("input[type='submit']")
-        .removeAttribute("disabled"));
+      : null);
   })
   console.info("login");
 }
