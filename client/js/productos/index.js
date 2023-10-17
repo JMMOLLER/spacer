fcf2323e2a6bf341c0d8592679971e2eda59b44f
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-10-17 17:59:41 -0500

    add 'productos' view

---
+++ client/js/productos/index.js
@@ -0,0 +1,7 @@
+import { loadHeaderLottieAnimation } from "../home/index.js";
+import Global from "../globals/index.js";
+const module = Global.getInstance();
+
+export default async function init() {
+  loadHeaderLottieAnimation();
+}
