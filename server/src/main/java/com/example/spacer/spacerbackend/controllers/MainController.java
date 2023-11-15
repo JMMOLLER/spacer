eaa3fbe14faf5e825309ea479ca61e3203166cfe
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2023-11-15 13:22:44 -0500

    Upgrade in custom Response class

--- server/src/main/java/com/example/spacer/spacerbackend/controllers/MainController.java
+++ server/src/main/java/com/example/spacer/spacerbackend/controllers/MainController.java
@@ -49,7 +49,7 @@ public class MainController {
 
   @GetMapping("/api")
   public ResponseEntity<?> apiHome() {
-    return new ResponseEntity<>(new Response(HttpStatus.OK, HttpStatus.OK.name(), "Welcome to Spacer API on v1.3.4 🚀!"), HttpStatus.OK);
+    return new ResponseEntity<>(new Response(HttpStatus.OK, HttpStatus.OK.name(), "Welcome to Spacer API on v1.3.5 🚀!"), HttpStatus.OK);
   }
 
   @PostMapping("/cliente/reset-password")
--- server/src/main/java/com/example/spacer/spacerbackend/services/Response.java
+++ server/src/main/java/com/example/spacer/spacerbackend/services/Response.java
@@ -1,7 +1,9 @@
 package com.example.spacer.spacerbackend.services;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 public class Response {
@@ -16,6 +18,56 @@ public class Response {
     this.statusCode = statusCode.value();
     this.description = message;
     this.response = response;
+  }
+
+  public Response(String description, Object response) {
+    this.description = description;
+    this.response = response;
+  }
+
+  public Response(String description) {
+    this.description = description;
+  }
+
+  public Response() {}
+
+  public ResponseEntity<Response> customResponse() {
+    return new ResponseEntity<>(this, HttpStatus.valueOf(this.statusCode));
+  }
+
+  public ResponseEntity<Response> badRequestResponse() {
+    var httpStatus = HttpStatus.BAD_REQUEST;
+    this.statusCode = httpStatus.value();
+    if(StringUtils.isBlank(this.description)) this.description = httpStatus.name();
+    return new ResponseEntity<>(this, httpStatus);
+  }
+
+  public ResponseEntity<Response> unauthorizedResponse() {
+    var httpStatus = HttpStatus.UNAUTHORIZED;
+    this.statusCode = httpStatus.value();
+    if(StringUtils.isBlank(this.description)) this.description = httpStatus.name();
+    return new ResponseEntity<>(this, httpStatus);
+  }
+
+  public ResponseEntity<Response> okResponse() {
+    var httpStatus = HttpStatus.OK;
+    this.statusCode = httpStatus.value();
+    if(StringUtils.isBlank(this.description)) this.description = httpStatus.name();
+    return new ResponseEntity<>(this, httpStatus);
+  }
+
+  public ResponseEntity<Response> createdResponse() {
+    var httpStatus = HttpStatus.CREATED;
+    this.statusCode = httpStatus.value();
+    if(StringUtils.isBlank(this.description)) this.description = httpStatus.name();
+    return new ResponseEntity<>(this, httpStatus);
+  }
+
+  public ResponseEntity<Response> internalServerErrorResponse() {
+    var httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    this.statusCode = httpStatus.value();
+    if(StringUtils.isBlank(this.description)) this.description = httpStatus.name();
+    return new ResponseEntity<>(this, httpStatus);
   }
 
 }
